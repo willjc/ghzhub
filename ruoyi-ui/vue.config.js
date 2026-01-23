bb69fa87fa5dcb2d7d51367f0765642b5b101acb
willjc <8863704@qq.com>
2026-01-24 03:17:42 +0800

    00000000000000000000

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -9,8 +9,8 @@ const CompressionPlugin = require('compression-webpack-plugin')
 
 const name = process.env.VUE_APP_TITLE || '港好住管理系统' // 网页标题
 
-// const baseUrl = 'http://ghzapi.dayushaiwang.com' // 后端接口
-const baseUrl = 'http://localhost:8090' // 后端接口
+const baseUrl = 'http://ghzapi.dayushaiwang.com' // 后端接口
+// const baseUrl = 'http://localhost:8090' // 后端接口
 
 const port = process.env.port || process.env.npm_config_port || 80 // 端口
 
--- sql/newgangzhu-0124.sql
+++ sql/newgangzhu-0124.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80015
 File Encoding         : 65001
 
-Date: 2026-01-24 03:07:03
+Date: 2026-01-24 03:16:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
