f1af2bf07d38fee21c2e4cb9ab0bae2496ab0c9e
wangwen <7566962+xszhensheng@user.noreply.gitee.com>
2026-05-06 16:41:27 +0800

    清理登陆账号和密码

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -87,8 +87,8 @@ export default {
       title: process.env.VUE_APP_TITLE,
       codeUrl: "",
       loginForm: {
-        username: "admin",
-        password: "admin123",
+        username: "",
+        password: "",
         rememberMe: false,
         code: "",
         uuid: ""
